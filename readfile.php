441ef747fd03bd7cec2724b7f6fc78825f6f567b
Nuria <nuria.villaronga@xercode.es>
2021-04-27 13:26:44 +0200

    Nuevos cambios readfile

--- readfile.php
+++ readfile.php
@@ -1,15 +1,31 @@
 <?php
+//Ejemplos de formas de hacerlo
 /*
 $fichero = file_get_contents ( './book.xml');
 echo $fichero;
 */
 
+/*
 function leer_fichero_completo($nombre_fichero){
     $fichero_texto = fopen ($nombre_fichero, "r");
     $contenido_fichero = fread($fichero_texto, filesize($nombre_fichero));
     fclose($fichero_texto);
     return $contenido_fichero;
 }
-echo leer_fichero_completo('./book.xml')
-//Sirven ambas
+echo leer_fichero_completo('./book.xml');
+*/
+
+ function leer_fichero_completo($nombre_fichero){
+    $fichero = file_get_contents ($nombre_fichero);
+    return $fichero;
+}
+//echo leer_fichero_completo('./book.xml')*/
+
+//-------------------
+//crea otra función que reciba como entrada el contenido del fichero y retorne un objeto xml
+function obxml($fichero){
+    $fichero2 = simplexml_load_string($fichero);
+    return $fichero2;
+}
+var_dump(obxml(leer_fichero_completo('./book.xml')));
 ?>
